a596ca449c294c37d4e660eaedf541d3e04a551e
cdils <cdils@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-06-02 18:57:42 +0000

    Bumping to 1.0.1
    git-svn-id: https://plugins.svn.wordpress.org/genesis-style-trump/tags/1.0.1@925317 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ genesis-style-trump.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Genesis Style Trump
+ *
+ * Read more about why I created this plugin at http://www.carriedils.com/woocommerce-genesis-important-style/
+ *
+ * @package           Genesis_Style_Trump
+ * @author            Carrie Dils
+ * @license           GPL-2.0+
+ * @link              http://www.carriedils.com
+ * @copyright         2013 Carrie Dils
+ *
+ * Plugin Name:       Genesis Style Trump
+ * Plugin URI:        http://wordpress.org/plugins/genesis-style-trump
+ * Description:       Loads Genesis child theme style sheet after plugin styles.
+ * Version:           1.0.1
+ * Author:            Carrie Dils
+ * Author URI:        http://www.carriedils.com
+ * Text Domain:       genesis-style-trump
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Domain Path:       /languages
+ * GitHub Plugin URI: https://github.com/cdils/genesis-style-trump
+ * GitHub Branch:     master
+ */
+
+// If this file is called directly, abort.
+if ( ! defined( 'ABSPATH' ) ) {
+    die;
+}
+
+add_action( 'genesis_setup', 'genesisstyletrump_load_stylesheet' );
+/**
+ * Move Genesis child theme style sheet to a much later priority to give any plugins a chance to load first.
+ *
+ * @since 1.0.0
+ */
+function genesisstyletrump_load_stylesheet() {
+
+	// If Parallax Pro theme is active, enqueue Genesis Style Trump at earlier priority
+	$priority = 'Parallax Pro Theme' == wp_get_theme() ? 14 : 999;
+
+	remove_action( 'genesis_meta', 'genesis_load_stylesheet' );
+	add_action( 'wp_enqueue_scripts', 'genesis_enqueue_main_stylesheet', $priority );
+}
